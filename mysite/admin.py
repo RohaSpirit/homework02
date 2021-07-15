709a29f352a0e8c64bee1764919efb4f2bdeacb6
RohaSpirit <malaysid00@gmail.com>
2021-07-15 23:16:41 +0800

    first commit

---
+++ mysite/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from mysite.models import Post, Country, City, Note
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date')
+
+
+admin.site.register(Post, PostAdmin)
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country_id')
+
+
+admin.site.register(Country, CountryAdmin)
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'population', 'country')
+
+
+admin.site.register(City, CityAdmin)
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date')
+admin.site.register(Note, NoteAdmin)
